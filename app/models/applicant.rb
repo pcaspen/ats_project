b9b44315f16b16c0700a38ea114aeb0e51ca55c9
Patty Laushman <plaushman@gmail.com>
2013-08-31 11:32:47 -0600

    Initial attempt at models files complete

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,4 +2,7 @@ class Applicant < ActiveRecord::Base
 	has_and_belongs_to_many :jobs, join_table: "applicants_jobs_statuses",
 	class_name: "Job", foreign_key: "applicant_id",
 	association_foreign_key: "job_id"
+	has_and_belongs_to_many :statuses, join_table: "applicants_jobs_statuses",
+	class_name: "Status", foreign_key: "applicant_id",
+	association_foreign_key: "status_id"
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,7 @@ class Status < ActiveRecord::Base
 	has_and_belongs_to_many :jobs, join_table: "applicants_jobs_statuses",
 	class_name: "Job", foreign_key: "status_id",
 	association_foreign_key: "job_id"
+	has_and_belongs_to_many :applicants, join_table: "applicants_jobs_statuses",
+	class_name: "Applicants", foreign_key: "status_id",
+	association_foreign_key: "applicant_id"
 end
